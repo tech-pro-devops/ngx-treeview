3963fb16b7ae5127f0d3e4c9a54668fcc29dae31
Mohammed Saif <saifansari0517@gmail.com>
2022-01-09 18:02:02 +0530

    Child Count feature support

--- projects/ngx-treeview-demo/src/app/app.module.ts
+++ projects/ngx-treeview-demo/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxTreeviewModule } from 'projects/ngx-treeview/src/public-api';
 
